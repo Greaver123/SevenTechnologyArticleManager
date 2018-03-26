ba53d9303ca9fd77165daf526d8598e09256f5cf
Greaver123 <p.kolodziej92@gmail.com>
2018-03-26 16:45:47 +0200

    Change name of main window

--- SevenTechnologyArticleManager/MainWindow.cs
+++ SevenTechnologyArticleManager/MainWindow.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SevenTechnologyArticleManager
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
